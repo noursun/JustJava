038fe5ea6340d0c23ac07b8b9b2bfdfb022b5aac
noursun <fn_mehadjibia@esi.dz>
2020-03-26 04:27:57 +0100

    finished

--- app/src/main/java/com/example/android/just_java/MainActivity.java
+++ app/src/main/java/com/example/android/just_java/MainActivity.java
@@ -22,8 +22,9 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        display(quantity);
-        displayPrice(quantity*5);
+        int price = quantity*5;
+        String message = "Total: $" + price + "\nThank you!";
+        displayMessage(message);
     }
 
     public void increment(View view){
@@ -47,5 +48,10 @@ public class MainActivity extends AppCompatActivity {
     private void displayPrice(int number) {
         TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
         priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
+    }
+
+    private void displayMessage(String message) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(message);
     }
 }
