1d1482ee7ff9cd4c1b0b6c0162d678057621c214
noursun <fn_mehadjibia@esi.dz>
2020-03-29 05:16:58 +0100

    Order summary

--- app/src/main/java/com/example/android/just_java/MainActivity.java
+++ app/src/main/java/com/example/android/just_java/MainActivity.java
@@ -3,6 +3,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.text.NumberFormat;
 
@@ -22,36 +23,40 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        int price = quantity*5;
-        String message = "Total: $" + price + "\nThank you!";
+        int price = calculatePrice(quantity,5);
+        String message = "Name : Nour el houda";
+        message += "\nQuantity : " + quantity;
+        message += "\nTotal: $" + price ;
+        message +="\nThank you!";
         displayMessage(message);
+    }
+
+    private int calculatePrice(int quantity, int pricePerCup) {
+        int price = quantity * pricePerCup;
+        return price;
     }
 
     public void increment(View view){
         quantity ++;
-        display(quantity);
+        displayQuantity(quantity);
     }
 
     public void decrement(View view){
         quantity --;
-        display(quantity);
+        displayQuantity(quantity);
     }
 
     /**
      * This method displays the given quantity value on the screen.
      */
-    private void display(int number) {
+    private void displayQuantity(int number) {
         TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
         quantityTextView.setText("" + number);
     }
 
-    private void displayPrice(int number) {
-        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
-        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
-    }
-
     private void displayMessage(String message) {
-        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
-        priceTextView.setText(message);
+        TextView orderSummaryTextView = (TextView) findViewById(R.id.order_summary_text_view);
+        orderSummaryTextView.setText(message);
+        Toast.makeText(this, "Order Summary", Toast.LENGTH_SHORT).show();
     }
 }
